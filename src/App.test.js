90f0840158c0b0198aa8207cb3305e0ab0563dc3
evndroo <evandro.slipjr@gmail.com>
2020-11-19 19:15:26 -0300

    adicionando novos testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,45 +1,70 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import App, { calcularNovoSaldo } from "./App"
+import { act } from 'react-dom/test-utils';
 
 
-describe("Componente principal", () =>{
-    describe("Quando eu abro o app do banco...", () =>{
-        it("o nome do banco deve ser exibido", ()=>{
-            render(<App/>)
+describe("Componente principal", () => {
+    describe("Quando eu abro o app do banco...", () => {
+        it("o nome do banco deve ser exibido", () => {
+            render(<App />)
             expect(screen.getByText("ByteBank")).toBeInTheDocument();
         })
 
-        it("o saldo é exibido", ()=>{
-            render(<App/>)
+        it("o saldo é exibido", () => {
+            render(<App />)
             expect(screen.getByText("Saldo:")).toBeInTheDocument();
         })
 
-        it("o botão de realizar operação é exibido", ()=>{
-            render(<App/>)
+        it("o botão de realizar operação é exibido", () => {
+            render(<App />)
             expect(screen.getByText("Realizar operação")).toBeInTheDocument();
         })
     })
-    describe("Quando realizo uma transalção", ()=>{
-        it("de saque, o saldo deve diminuir", ()=>{
+    describe("Quando realizo uma transação", () => {
+        it("de saque, o saldo deve diminuir", () => {
             const valores = {
                 transacao: "saque",
                 valor: 100
             };
 
-            const novoSaldo = calcularNovoSaldo(valores,200);
-            
+            const novoSaldo = calcularNovoSaldo(valores, 200);
+
             expect(novoSaldo).toBe(100)
-        })
-        
-        it("de depósito", ()=>{
+        });
+
+        it("de saque, a transação deve ser realizada", () => {
+            act(() => {
+                const { getByText, getByTestId, getByLabelText } = render(<App />)
+
+
+                const saldo = getByText("R$ 1000");
+                const transacao = getByLabelText("Saque");
+                const valor = getByTestId("valor");
+                const botaoTransacao = getByText("Realizar operação");
+
+
+                expect(saldo.textContent).toBe("R$ 1000");
+                fireEvent.click(transacao, { target: { value: "saque" } });
+
+                fireEvent.change(valor, { target: { value: "10" } });
+
+                fireEvent.click(botaoTransacao);
+
+                expect(saldo.textContent).toBe("R$ 990");
+
+            });
+        });
+
+
+        it("de depósito, o saldo deve aumentar", () => {
             const valores = {
                 transacao: "deposito",
                 valor: 100
             };
 
-            const novoSaldo = calcularNovoSaldo(valores,200);
-            
+            const novoSaldo = calcularNovoSaldo(valores, 200);
+
             expect(novoSaldo).toBe(300)
         });
     })
